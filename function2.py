a1068589fae8e76835e8696149910b8257669626
winston750810 <ksw3695@naver.com>
2022-10-12 16:23:04 +0900

    함수연습3

--- function2.py
+++ function2.py
@@ -34,3 +34,27 @@ def func2(a):
     
 #호출
 print(func2(1))
+
+#참조를 전달하는 방식(입력 + 출력)
+wordlist = ["J", "A" , "M"]
+def change(x):
+    x[0] = "H"
+
+#호출
+change(wordlist)
+print("함수 호출후 : {0}".format(wordlist))
+
+
+print("-------복사본 사용-------")
+#복사본 사용(원본을 수정하기 싫을 때)
+wordlist = ["J", "A" , "M"]
+def change(x):
+    #Deep copy(진짜 복사본 사용) : 지역변수
+    x1=x[:]
+    x1[0]="H"
+    print("함수 내부 : {0}".format(x1))
+
+#호출
+change(wordlist)
+#원본을 복사해서 사용했기 때문에 원본은 그대로 JAM 출력
+print("함수 호출후 : {0}".format(wordlist))
