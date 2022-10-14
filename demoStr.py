25d6776fb66c8b9d2089fc8b297d157b88dda73d
winston750810 <ksw3695@naver.com>
2022-10-14 12:19:25 +0900

    문자열 연습1

---
+++ demoStr.py
@@ -0,0 +1,16 @@
+# demoStr.py
+
+strA = "파이썬은 무지 강력해"
+strB = "python is very powerful"
+
+print(len(strA))
+print(len(strB))
+
+print(strB.capitalize() )
+print(strB.upper() )
+
+print(strB.count("p") )
+print(strB.count("p" , 7) )
+print("MBC2580".isalnum())
+print("MBC:2580".isalnum())
+print("2580".isnumeric())
